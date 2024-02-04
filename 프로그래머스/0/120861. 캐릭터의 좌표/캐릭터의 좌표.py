b1e0cd34fce09fb6e12e702fb550ae6fe5654fed
Cheong-Myeong <156451335+Cheong-Myeong@users.noreply.github.com>
2024-02-05 01:51:54 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -0,0 +1,20 @@
+def solution(keyinput, board):
+       # 왼, 오, 위, 아래
+    di = [-1, 1, 0, 0]
+    dj = [0, 0, 1, -1]
+    answer = [0, 0]
+    
+    for key in keyinput:
+        if key == 'left':
+            if -(board[0]//2) <= answer[0] + di[0] <= (board[0]//2):
+                answer[0] += di[0]
+        if key == 'right':
+            if -(board[0]//2) <= answer[0] + di[1] <= (board[0]//2):
+                answer[0] += di[1]
+        if key == 'up':
+            if -(board[1]//2) <= answer[1] + dj[2] <= (board[1]//2):
+                answer[1] += dj[2]
+        if key == 'down':   
+            if -(board[1]//2) <= answer[1] + dj[3] <= (board[1]//2):
+                answer[1] += dj[3]
+    return answer        
